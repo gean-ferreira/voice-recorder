2f64625cf682e72742e278b0bbc773a4fffc68b2
Gean Ferreira <gean_roberto@hotmail.com>
2021-10-06 00:20:03 -0300

    MediaDevices.getUserMedia() + testing buttons

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Voice recoder using Svelte framework.
 
 For styling Sveltstrap will be used (sveltestrap.js.org).
+
+Get audio track from user microphone via MediaDevices.getUserMedia() method.
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,28 @@
 <script>
+  let mediaRecorder = null;
+
+  const constrains = { audio: true };
+  navigator.mediaDevices.getUserMedia(constrains).then((stream) => {
+    mediaRecorder = new MediaRecorder(stream);
+  });
+
+  function record() {
+    mediaRecorder.start();
+    console.log("play button has been activated");
+  }
+
+  function stop() {
+    mediaRecorder.stop();
+    console.log("stop button has been activated");
+  }
 </script>
 
 <body>
-  <h1>Voice recoder</h1>
+  <h1>Voice recorder</h1>
 
   <div class="buttons">
-    <button>Play</button>
-    <button>Stop</button>
+    <button on:click={record}>Play</button>
+    <button on:click={stop}>Stop</button>
     <span>Waiting</span>
     <span>Recording</span>
   </div>
